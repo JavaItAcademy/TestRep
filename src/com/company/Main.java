c3f6c7481429a8efc9b66c74d8afedd6697e6071
Askhat <serikov.askhat@gmail.com>
2019-01-10 20:34:38 +0600

    First

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello");
     }
 }
