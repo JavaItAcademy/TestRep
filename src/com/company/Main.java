641140bee93faeaed290205390636fb8fea80b25
Askhat <serikov.askhat@gmail.com>
2019-01-12 20:47:14 +0600

    New Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello There general Kenobi");
+	// write your code here
     }
 }
