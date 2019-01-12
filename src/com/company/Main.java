488fce77675460715d6a680be03f5fcddaf5441c
Askhat <serikov.askhat@gmail.com>
2019-01-12 20:44:14 +0600

    Brand new master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello There general Kenobi");
     }
 }
