257717c83a57cd0eb96313abed077bfb40b21f6b
AseliaA <aselia.azimkanova@gmail.com>
2019-01-22 18:32:28 +0600

    Added txt file

---
+++ src/com/company/Aselia/Aselia.java
@@ -0,0 +1,4 @@
+package com.company.Aselia;
+
+public class Aselia {
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    public Connection connect(){
+        final String url = "jdbc:postgresql://localhost:5432/";
+        final String login = "postgres";
+        final String password = "umarlove";
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url , login , password);
+            System.out.println("Connected to the PostgreSQl server successfully.");
+        }catch (SQLException e ){
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 }
